5cd1d3e4503c8ff6f0425d683a915491432d47c8
Michelle Chen <michelle.chen@iit.com.hk>
2020-09-01 16:16:49 +0800

    Initial Commit

---
+++ DoNotLeaveEarlier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DoNotLeaveEarlier
+//
+//  Created by michelle on 2020/9/1.
+//  Copyright © 2020 michelle. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
