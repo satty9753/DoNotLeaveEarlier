60f5b86c248537b2cf38cdb21ecb969520235a7d
Michelle Chen <michelle.chen@iit.com.hk>
2020-09-04 14:47:33 +0800

    1. add month cell

---
+++ DoNotLeaveEarlier/DateItem.swift
@@ -0,0 +1,21 @@
+//
+//  DateItem.swift
+//  DoNotLeaveEarlier
+//
+//  Created by michelle on 2020/9/4.
+//  Copyright © 2020 michelle. All rights reserved.
+//
+
+import Cocoa
+
+class DateItem: NSCollectionViewItem {
+   
+    @IBOutlet weak var dateLabel: NSTextFieldCell!
+    @IBOutlet weak var timeLabel: NSTextFieldCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
--- DoNotLeaveEarlier/VC/MonthVC.swift
+++ DoNotLeaveEarlier/VC/MonthVC.swift
@@ -13,10 +13,45 @@ class MonthVC: NSViewController {
     @IBOutlet weak var collectionView: NSCollectionView!
     @IBOutlet weak var previousButton: NSButton!
     @IBOutlet weak var nextButton: NSButton!
+    
+    var calenderManager: CalenderManager?
+    
+    let cellId = NSUserInterfaceItemIdentifier("DateItem")
+    
+    var days:[Day]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        let date = Date()
+        
+        titleLabel.title = date.toString(format: "YYYY-MM")
+        calenderManager = CalenderManager(baseDate: date)
+        days = calenderManager?.generateDaysInMonth(for: date)
+
+        collectionView.register(DateItem.self, forItemWithIdentifier: cellId)
     }
     
+}
+
+
+extension MonthVC: NSCollectionViewDelegate, NSCollectionViewDataSource{
+    
+    func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
+        let cell = collectionView.makeItem(withIdentifier: cellId, for: indexPath) as? DateItem
+        cell?.dateLabel.title = days?[indexPath.item].number ?? ""
+        return cell!
+    }
+    
+    func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        return days?.count ?? 0
+    }
+}
+
+
+extension MonthVC: NSCollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> NSSize {
+        return NSSize(width: collectionView.frame.width/8.0, height: collectionView.frame.height/5.0)
+    }
 }
